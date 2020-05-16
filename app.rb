860ea9c22fae9e7f9ad34e195d580f83d78515ac
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2020-05-16 09:21:42 -0300

    Delete players and store game's scores
    This commit implements a few features:
    - It's now possible to delete players.
    - Now we can save or update the results of the games from /games.
    - The results of the games are shown at /show_results.

--- app.rb
+++ app.rb
@@ -11,14 +11,25 @@ Cuba.plugin Cuba::Mote
 
 Cuba.define do
   on post do
+
+    on "delete_player", param("player_for_delete") do |player|
+      conn.exec_params("delete from players where name =($1)",
+        [player])
+      res.redirect("/players")
+    end
+
+
     on "scores" do
-      res.write "To be done"
-      # score = req.params.fetch("score")
+      on param("id1"), param("id2"), param("score1"), param("score2") do |id1, id2, score1, score2|
+        conn.exec_params("insert into games (id_player_1, id_player_2, score_1, score_2) values ($1, $2, $3, $4) on conflict(id_player_1, id_player_2) do update set score_1 = $3, score_2 = $4", [id1, id2, score1, score2])
+        res.redirect("/games")
+      end
+      res.redirect("/games")
     end
 
     on "create_new_player", param("player1") do |new_player|
-       conn.exec_params("insert into players (name) values ($1)", [new_player])
-       res.redirect("/players")
+      conn.exec_params("insert into players (name) values ($1)", [new_player])
+      res.redirect("/players")
     end
   end
 
@@ -37,19 +48,43 @@ Cuba.define do
 
     on "games" do
       games = []
-      sql = "select p1.name as p1_name, p2.name as p2_name, p1.id as p1_id, p2.id as p2_id from players p1 join players p2 on p1.id < p2.id"
+      sql = "select p1.name as p1_name,
+        p2.name as p2_name,
+        p1.id as p1_id,
+        p2.id as p2_id,
+        games.score_1,
+        games.score_2
+        from players p1
+        join players p2 on p1.id < p2.id
+        left join games on p1.id = games.id_player_1 and p2.id = games.id_player_2"
       conn.exec(sql) do |result| 
         result.each do |row|
           games << row
         end
       end
-
       render("games", games: games)
     end
 
+    on "show_results" do
+      results = []
+      sql = "select p1.name as p1_name,
+        p2.name as p2_name,
+        p1.id as p1_id,
+        p2.id as p2_id,
+        games.score_1,
+        games.score_2
+        from players p1
+        join players p2 on p1.id < p2.id
+        join games on p1.id = games.id_player_1 and p2.id = games.id_player_2"
+      conn.exec(sql) do |result| 
+        result.each do |row|
+          results << row
+        end
+      end
+      render("show_results", results: results)
+    end
     on root do
       res.redirect("/games")
     end
-
   end
 end
