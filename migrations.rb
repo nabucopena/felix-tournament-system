8c7c242268899f7225c38ac2f71363b96f4ee591
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2020-05-21 17:28:24 -0300

    Add migrations file

--- migrations.rb
+++ migrations.rb
@@ -4,6 +4,9 @@ conn = if ENV["DATABASE_URL"]
        else
          PG.connect(dbname: "felix")
        end
+def getnumber(str)
+	str.split("_").first.to_i
+end
 
 conn.exec("create table if not exists migrations
 	(file text not null)")
@@ -12,8 +15,7 @@ executed_migrations = conn.exec("select file from migrations").
 	to_a.
 	map { |item| item.fetch("file")}
 migrations_to_run = migrations-executed_migrations
-migrations_to_run.sort.each do |filename|
+migrations_to_run.sort_by{ |item| getnumber(item)}.each do |filename|
 	conn.exec(File.read("#{__dir__}/migrations/#{filename}"))
 	conn.exec_params("insert into migrations(name) values($1)", filename)
 end
-
