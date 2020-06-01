5a490d5386477e7888116e2b0ce6d07401a87033
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2020-06-01 01:16:10 -0300

    Models

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "cuba/mote"
 require "pg"
 require "bcrypt"
 require "./db_connection"
+require "./models.rb"
 DBConnection.connect
 module Helpers
   def current_user
@@ -32,22 +33,12 @@ Cuba.define do
     end
     on post do
       on param("username"), param("password") do |user, password|
-        result = conn.exec_params("select
-          username, password
-          from users
-          where username=$1",
-        [user]).first
-        on result do
-          on BCrypt::Password.new(result.fetch("password"))==password do
-            session[:user]=user
-            res.redirect("/games")
-          end
-          session[:error] = "Invalid password"
-          res.redirect("/login")
-        end
-        session[:error] = "Invalid username"
-        res.redirect("/login")
+        result = Accounts.check_login(user, password)
+        session[:error] = result[:error]
+        session[:user] = result[:user]
+        res.redirect("/games")
       end
+      session[:error] = "All parameters required"
       res.redirect("/login")
     end
   end
@@ -59,15 +50,9 @@ Cuba.define do
 
   on post, "register_user" do
     on param("user"), param("password") do |user, password|
-      used_name = conn.exec_params("select count(*) from users where username=$1", [user])
-      on used_name[0].fetch("count").to_i>0 do
-        session[:error] = "This username is already registered"
-        res.redirect("/register")
-      end
-      conn.exec_params("insert into users
-        (username, password) values ($1, $2)",
-        [user, BCrypt::Password.create(password)])
-      session[:user]=user
+      result = Accounts.create_account(user, password)
+      session[:error] = result[:error]
+      session[:user] = result[:user]
       res.redirect("/games")
     end
     session[:error] = "All parameters required"
@@ -81,17 +66,14 @@ Cuba.define do
   on !!current_user do
     on post do
       on "delete_player", param("player_for_delete") do |player|
-        conn.exec_params("delete from players where id =($1)",
-          [player])
-        conn.exec_params("delete from games where id_player_1 =($1) or id_player_2=($1)",
-          [player])
+        Games.delete_player(player)
         res.redirect("/players")
       end
 
 
       on "scores" do
         on param("id1"), param("id2"), param("score1"), param("score2") do |id1, id2, score1, score2|
-          conn.exec_params("insert into games (id_player_1, id_player_2, score_1, score_2) values ($1, $2, $3, $4) on conflict(id_player_1, id_player_2) do update set score_1 = $3, score_2 = $4", [id1, id2, score1, score2])
+          Games.set_scores(id1: id1, id2: id2, score1: score1, score2: score2)
           res.redirect("/games")
         end
         session[:error] = "Scores are required"
@@ -99,50 +81,26 @@ Cuba.define do
       end
 
       on "create_new_player", param("player1") do |new_player|
-        conn.exec_params("insert into players (name) values ($1)", [new_player])
+        Games.create_player(new_player)
         res.redirect("/players")
       end
     end
 
     on get do
       on "positions" do
-        sql = <<~SQL
-          select name,
-          count(*) filter(where my_id is not null) as played,
-          count(*) filter(where id=my_id and my_score>opponent_score) as won
-          from players
-          left join my_games on my_id=id
-          group by id;
-          SQL
-        players = conn.exec(sql)
+        players = Games.list_positions
         render("positions", {players: players})
       end
 
       on "players" do
         on root do
-          players = []
-          conn.exec("select name, id from players") do |result|
-            result.each do |row|
-              players << row
-            end
-          end
+          players = Games.list_players
           render "players", players: players
         end
         on ":id" do |id|
-          player = conn.exec_params(
-            "select name from players where id=$1", [id]
-            ).first
+          player = Games.get_player_name_by_id(id)
           on player do
-            games = conn.exec_params(
-              "select my_score as score,
-              opponent_score,
-              name as opponent
-              from my_games
-              join players
-              on opponent_id=id
-              where my_id=$1",
-              [id]
-              )
+            games = Games.list_player_games(id)
             render "player", {
               player: player,
               games:games
@@ -152,40 +110,12 @@ Cuba.define do
       end
 
       on "games" do
-        games = []
-        sql = "select p1.name as p1_name,
-          p2.name as p2_name,
-          p1.id as p1_id,
-          p2.id as p2_id,
-          games.score_1,
-          games.score_2
-          from players p1
-          join players p2 on p1.id < p2.id
-          left join games on p1.id = games.id_player_1 and p2.id = games.id_player_2"
-        conn.exec(sql) do |result| 
-          result.each do |row|
-            games << row
-          end
-        end
+        games = Games.fixture
         render("games", games: games)
       end
 
       on "show_results" do
-        results = []
-        sql = "select p1.name as p1_name,
-          p2.name as p2_name,
-          p1.id as p1_id,
-          p2.id as p2_id,
-          games.score_1,
-          games.score_2
-          from players p1
-          join players p2 on p1.id < p2.id
-          join games on p1.id = games.id_player_1 and p2.id = games.id_player_2"
-        conn.exec(sql) do |result| 
-          result.each do |row|
-            results << row
-          end
-        end
+        results = Games.list_games
         render("show_results", results: results)
       end
     end
---
+++ models.rb
@@ -0,0 +1,128 @@
+require "bcrypt"
+
+class Games
+  def self.create_player(new_player)
+    $conn.exec_params(
+      "insert into players (name) values ($1)",
+      [new_player]
+    )
+  end
+
+  def self.delete_player(player)
+    $conn.exec_params(
+      "delete from players where id =($1)",
+      [player]
+    )
+  end
+
+  def self.list_players
+    $conn.exec("select name, id from players").to_a
+  end
+
+  def self.list_player_games(player_id)
+  	$conn.exec_params(
+      "select my_score as score,
+      opponent_score,
+      name as opponent
+      from my_games
+      join players
+      on opponent_id=id
+      where my_id=$1",
+      [player_id]
+    ).to_a
+  end
+
+  def self.get_player_name_by_id(player_id)
+  	$conn.exec_params(
+      "select name from players where id=$1", [player_id]
+    ).first
+  end
+
+  def self.list_games
+    sql = "select p1.name as p1_name,
+      p2.name as p2_name,
+      p1.id as p1_id,
+      p2.id as p2_id,
+      games.score_1,
+      games.score_2
+      from players p1
+      join players p2 on p1.id < p2.id
+      join games on p1.id = games.id_player_1 and p2.id = games.id_player_2"
+    $conn.exec(sql).to_a
+  end
+
+  def self.fixture
+    sql = "select p1.name as p1_name,
+      p2.name as p2_name,
+      p1.id as p1_id,
+      p2.id as p2_id,
+      games.score_1,
+      games.score_2
+      from players p1
+      join players p2 on p1.id < p2.id
+      left join games on p1.id = games.id_player_1 and p2.id = games.id_player_2"
+    $conn.exec(sql).to_a
+  end
+
+  def self.set_scores(id1:, id2:, score1:, score2:)
+    $conn.exec_params("insert into games (
+    	id_player_1,
+    	id_player_2,
+    	score_1,
+    	score_2
+    	)
+    	values ($1, $2, $3, $4)
+    	on conflict(id_player_1, id_player_2)
+    	do
+    	update set score_1 = $3, score_2 = $4",
+    	[id1, id2, score1, score2])
+  end
+
+  def self.list_positions
+  	sql = <<~SQL
+      select name,
+      count(*) filter(where my_id is not null) as played,
+      count(*) filter(where id=my_id and my_score>opponent_score) as won
+      from players
+      left join my_games on my_id=id
+      group by id;
+      SQL
+    $conn.exec(sql).to_a
+  end
+end
+
+class Accounts
+  def self.check_login(user, password)
+    result = $conn.exec_params("select
+      username, password
+      from users
+      where username=$1",
+      [user]).first
+    if result
+      if BCrypt::Password.new(result.fetch("password"))==password
+        {user: user}
+      else
+        {error: "Invalid password"}
+      end
+    else
+      {error: "Invalid username"}
+    end
+  end
+
+
+  def self.create_account(user, password)
+    used_name = $conn.exec_params("select count(*)
+      from users
+      where username=$1",
+      [user]).first.fetch("count").to_i
+    if used_name>0
+      {error: "This username is already registered"}
+    else
+      encrypted = BCrypt::Password.create(password)
+      $conn.exec_params("insert into users
+        (username, password) values ($1, $2)",
+        [user, encrypted])
+      {user: user}
+    end
+  end
+end
